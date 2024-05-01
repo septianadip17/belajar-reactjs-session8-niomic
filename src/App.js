aa8782d215cd7d1a153a56b9699917e931d322dc
iay <septianadip7998@gmail.com>
2024-05-02 06:12:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Carousel, Row, Col, Dropdown } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Row>
+        <Col>
+          <h1 className="text-center">Belajar 8</h1>
+        </Col>
+        <Col>
+          <Dropdown>
+            <Dropdown.Toggle variant="success">Pilih Maskapai</Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="/">Garuda Indonesia</Dropdown.Item>
+              <Dropdown.Item href="/">Pelita Air</Dropdown.Item>
+              <Dropdown.Item href="/">Air Asia</Dropdown.Item>
+              <Dropdown.Item href="/">Super Jet</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/travel1.jpeg"
+                alt="travel1"
+                className="d-block w-100"
+              />
+              <Carousel.Caption>
+                <h3>Pesawat Tiket Murah 1</h3>
+                <p>Dapatkan Promo Tiket Pesawat Murah 1</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/travel2.png"
+                alt="travel2"
+                className="d-block w-100"
+              />
+              <Carousel.Caption>
+                <h3>Pesawat Tiket Murah 2</h3>
+                <p>Dapatkan Promo Tiket Pesawat Murah 2</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/travel3.jpeg"
+                alt="travel3"
+                className="d-block w-100"
+              />
+              <Carousel.Caption>
+                <h3>Pesawat Tiket Murah 3</h3>
+                <p>Dapatkan Promo Tiket Pesawat Murah 3</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+        <Col>
+          <h1>pilih aja</h1>
+        </Col>
+      </Row>
     </div>
   );
 }
